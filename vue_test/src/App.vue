91a5ee81d8acf180d6c29fcc1acccea86428fdca
乐乐乐乐培 <LWP13226968149@163.com>
2021-09-24 21:38:28 +0800

    props配置

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,29 +1,17 @@
 <template>
 	<div>
-		<h1 v-text="msg" ref="title"></h1>
-		<button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
-		<School ref="sch"/>
+		<Student name="李四" sex="女" :age="18"/><!-- 这里传入组件格式没有逗号 是空格 -->
+		<Student name="乐培" sex="男" :age="20"/><!-- 这里传入的类型是字符串 不能去掉字符串(破坏结构)-->
+		<Student name="伟培" sex="男" :age="18+1"/><!-- v-bind=后是js表达式,不是字符串,但是仍然不能去掉双引号破坏结构-->
 	</div>
 </template>
 
 <script>
 	//引入School组件
-	import School from './components/School'
+	import Student from './components/Student'
 
 	export default {
 		name:'App',
-		components:{School},
-		data() {
-			return {
-				msg:'欢迎学习Vue！'
-			}
-		},
-		methods: {
-			showDOM(){
-				console.log(this.$refs.title) //真实DOM元素
-				console.log(this.$refs.btn) //真实DOM元素
-				console.log(this.$refs.sch) //School组件的实例对象（vc）
-			}
-		},
+		components:{Student},
 	}
 </script>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<h1>{{msg}}</h1>
+		<h2>学生姓名：{{name}}</h2>
+		<h2>学生姓名：{{sex}}</h2>
+		<h2>学生年龄：{{age}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Student',
+		data() {
+			return {
+				msg:'我是河南大学的一个学生',
+				// name:'乐培',
+				// sex:'男',
+				// age:18
+			}
+		},
+		// props:['name','sex','age'] //简单声明接收
+
+		//接收的同时对数据进行类型限制
+		// props:{
+		// 	name:String,
+		// 	age:Number,
+		// 	sex:String
+		// }
+
+		//接收的同时对数据：进行类型限制+默认值的指定+必要性的限制
+		props:{
+			name:{
+				type:String, //name的类型是字符串
+				required:true//name是必要的
+				//required和default一般不会同时出现
+			},
+			age:{
+				type:Number,
+				default:99	//默认值
+			},
+			sex:{
+				type:String,
+				required:true
+			}
+		}
+	}
+</script>
