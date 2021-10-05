70dde1dbe7b99a44c53c73cf6cb97ca0bb7fcead
乐乐乐乐培 <LWP13226968149@163.com>
2021-10-05 18:53:03 +0800

    vuex求和案例

---
+++ vue_test/25_src_求和案例_vuex版/store/index.js
@@ -0,0 +1,56 @@
+//该文件用于创建Vuex中最为核心的store
+
+import Vue from 'vue'
+
+//引入Vuex
+import Vuex from 'vuex'
+//应用Vuex插件
+Vue.use(Vuex)
+
+//准备actions--用于响应组件中的动作
+const actions = {
+    add(context,value){     //上下文对象
+        console.log('action中的add被调用了',context,value)
+        context.commit('Add',value) //作大写区分,编程习惯
+    },
+    reduce(context,value){
+        context.commit('Reduce',value)
+    },
+    addOdd(context,value){     //上下文对象
+        console.log('action中的add被调用了',context,value)
+        if(context.state.sum % 2){
+            context.commit('Add',value) 
+        }
+    },
+    addWait(context,value){
+        setTimeout(()=>{
+            context.commit('Add',value)
+        },500)
+    },
+}
+
+//准备mutations--用于操作数据(state)
+const mutations = {
+    Add(state,value){
+        console.log('mutation中的Add被调用了',state,value)
+        state.sum += value
+    },
+    Reduce(state,value){
+        state.sum -= value
+    },
+}
+
+//准备state--用于存储数据
+const state = {
+    sum:0
+}
+
+//创建并暴露store
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state,
+})
+
+// // 暴露“导出”store
+// export default store
