a3485814ae00ef60a8eda73611b44000b61fd48e
乐乐乐乐培 <LWP13226968149@163.com>
2021-10-07 23:32:31 +0800

    组件内路由守卫 Enter Leave

---
+++ vue_test/42_src_组件内路由守卫/pages/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>About组件内容</h2>
+</template>
+
+<script>
+    export default {
+        name:'About',
+        beforeDestroy() {
+          console.log('About组件即将被销毁了')
+        },
+        //通过路由规则,进入该组件时被调用
+        beforeRouteEnter (to, from, next) {
+          // console.log('About---beforeRouteEnter',to)
+          console.log('About---beforeRouteEnter',to,from)
+          if(to.meta.isAuth){//判断是否需要鉴权
+              if(localStorage.getItem('school') === 'henu'){
+                  // document.title = to.meta.title
+                  next()
+              }else{
+                  alert('学校名不对,无权限查看!')
+              }
+          }else{
+              // document.title = to.meta.title
+              next()
+          }
+        },
+
+        //通过路由规则,离开该组件时被调用
+        beforeRouteLeave (to, from,next) {
+          console.log('About---beforeRouteLeave',to,from)
+          next()
+        }
+    }
+</script>
+
