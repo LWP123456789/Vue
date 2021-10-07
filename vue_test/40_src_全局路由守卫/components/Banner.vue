fb4015c5c7027e55685d6963addb7d2cf3c0ec9c
乐乐乐乐培 <LWP13226968149@163.com>
2021-10-07 23:05:27 +0800

    前置路由守卫(常用) 后置路由守卫(处理路由切换后的一些细节)

---
+++ vue_test/40_src_全局路由守卫/components/Banner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-xs-offset-2 col-xs-8">
+		<div class="page-header"><h2>Vue Router Demo</h2></div>
+        <button @click="back">后退</button>
+        <button @click="forward">前进</button>
+	</div>
+</template>
+
+<script>
+    export default {
+        name:'Banner',
+        methods: {
+            back(){ 
+                // this.$router.back()
+                this.$router.go(-1) //go中的参数表示前进/后退几步
+            },
+            forward(){
+                this.$router.forward()
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
